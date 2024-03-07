c84c1d81f2d6c9bd66f8d102b35706b46ce2018b
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-07 06:04:55 -0500

    front end construction message added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-
 function App() {
+  const constructionMessageStyle = {
+    textAlign: 'center',
+    fontSize: '18px',
+    color: '#555',
+    marginTop: '20px',
+  };
 
   return (
     <div>
-        Hello world
+      <h1>PayTM App Frontend</h1>
+      <p style={constructionMessageStyle}>
+        This website is currently under construction. Please check back later for updates.
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
